c001a6c5c9ad91a041c9a6941f498c2630c39aa6
Claudio Almeida <cmma89@gmail.com>
2018-08-03 19:18:42 +0100

    Created Project

---
+++ src/main/java/nos/inverview/restfullapinos/RestfullApiNosApplication.java
@@ -0,0 +1,12 @@
+package nos.inverview.restfullapinos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfullApiNosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfullApiNosApplication.class, args);
+    }
+}
